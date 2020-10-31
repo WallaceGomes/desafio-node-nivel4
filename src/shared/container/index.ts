9c1ce6d712341f70d7f328213b926cca15012034
WallaceGomes <wallacecardosogomes@gmail.com>
2020-10-31 18:38:13 -0300

    chore(DB): added migrations

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -9,4 +9,18 @@ import ProductsRepository from '@modules/products/infra/typeorm/repositories/Pro
 import IOrdersRepository from '@modules/orders/repositories/IOrdersRepository';
 import OrdersRepository from '@modules/orders/infra/typeorm/repositories/OrdersRepository';
 
-// TODO
+container.registerSingleton<ICustomersRepository>(
+  'CustomerRepository',
+  CustomersRepository,
+);
+
+container.registerSingleton<IProductsRepository>(
+  'CustomerRepository',
+  ProductsRepository,
+);
+
+container.registerSingleton<IOrdersRepository>(
+  'CustomerRepository',
+  OrdersRepository,
+);
+
